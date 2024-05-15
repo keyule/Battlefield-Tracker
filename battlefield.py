2ff29bc200b8b93fb0d23e1cd7d91be45ba00529
yule <keyule@gmail.com>
2024-05-16 01:31:09 +0800

    prints rewards

--- battlefield.py
+++ battlefield.py
@@ -20,6 +20,10 @@ REGION_MAP = {0: "Pirate", 1: "Cat", 2: "Wolf", 3: "Food"}
 # Initialize request-id in memory
 current_request_id = int(os.getenv('REQUEST_ID'))
 
+# Load rewards data
+with open('rewards.json', 'r') as file:
+    rewards_data = json.load(file)
+
 def get_battlefields():
     """Fetch and return battlefield data from the API, incrementing request-id."""
     global current_request_id
@@ -48,22 +52,20 @@ def get_battlefields():
 
     return json.loads(response.text)
 
-def calculate_time_details(gen_time_str, disp_time_str):
+def calculate_time_details(disp_time_str):
     """Convert and calculate time details."""
     singapore_tz = pytz.timezone('Asia/Singapore')
     current_time = datetime.now(timezone.utc).astimezone(singapore_tz)
-    gen_time = datetime.fromisoformat(gen_time_str[:-1]).replace(tzinfo=timezone.utc).astimezone(singapore_tz)
     disp_time = datetime.fromisoformat(disp_time_str[:-1]).replace(tzinfo=timezone.utc).astimezone(singapore_tz)
-
     time_left = disp_time - current_time
     hours_left = time_left.seconds // 3600
     minutes_left = (time_left.seconds % 3600) // 60
     time_left_str = f"{hours_left} hrs {minutes_left} min" if hours_left > 0 else f"{minutes_left} min"
 
-    return gen_time.strftime('%m-%d %H:%M:%S'), disp_time.strftime('%m-%d %H:%M:%S'), time_left_str
+    return disp_time.strftime('%m-%d %H:%M:%S'), time_left_str
 
-def alert_for_new_mobs(new_mobs, data, min_time_left, reward_ids, telegram_alerts_enabled):
-    """Alerts if new mobs meet the criteria of time left, reward IDs, and includes region and level information."""
+def alert_for_new_mobs(new_mobs, data, min_time_left, reward_ids, telegram_alerts_enabled, rewards_data):
+    """Alerts if new mobs meet the criteria of time left, reward IDs, and includes region, level, and prize information."""
     singapore_tz = pytz.timezone('Asia/Singapore')
     current_time = datetime.now(timezone.utc).astimezone(singapore_tz)
     alert_mobs = []
@@ -73,44 +75,61 @@ def alert_for_new_mobs(new_mobs, data, min_time_left, reward_ids, telegram_alert
             region_name = REGION_MAP.get(region['region'], "Unknown")
             for battlefield in region["battlefields"]:
                 if battlefield['id'] == mob:
-                    gen_time_str, disp_time_str, time_left_str = calculate_time_details(battlefield["generatedTime"], battlefield["disappearedTime"])
+                    disp_time_str, time_left_str = calculate_time_details(battlefield["disappearedTime"])
                     disp_time = datetime.fromisoformat(battlefield["disappearedTime"][:-1]).replace(tzinfo=timezone.utc).astimezone(singapore_tz)
                     time_left = disp_time - current_time
                     minutes_left = (time_left.seconds // 60) + time_left.days * 1440  # Convert days to minutes if any
 
+                    # Handle first place prize
+                    reward_group_id = battlefield['rewardGroupId']
+                    if str(reward_group_id) in rewards_data and rewards_data[str(reward_group_id)]:
+                        first_place_prize = ', '.join(rewards_data[str(reward_group_id)][0])
+                    else:
+                        first_place_prize = "No data available"
+
                     if minutes_left < min_time_left and (not reward_ids or battlefield['rewardGroupId'] in reward_ids):
                         alert_message = (
-                            f"Alert: New mob with ID: {battlefield['id']}, "
-                            f"Region: {region['region']} - {region_name}, "
-                            f"Level: {battlefield['level']}, "
+                            f"Alert: New mob Spawned! \n ID: {battlefield['id']}\n "
+                            f"Region: {region_name}\n "
+                            f"Level: {battlefield['level']}\n "
+                            f"Time Left: {time_left_str}\n"
                             f"Reward Group ID: {battlefield['rewardGroupId']}, "
-                            f"Time Left: {minutes_left} minutes has spawned!"
+                            f"1st Prize: {first_place_prize}"
+                           
                         )
                         print(alert_message)
                         if telegram_alerts_enabled:
                             send_telegram_message(alert_message)
 
-
-def print_battlefield_info(data, previous_ids, min_time_left, reward_ids, telegram_alerts_enabled):
+def print_battlefield_info(data, previous_ids, min_time_left, reward_ids, telegram_alerts_enabled, rewards_data):
     table = PrettyTable()
-    table.field_names = ["ID", "Region", "Level", "Generated Time (SGT)", "Disappearing Time (SGT)", "Time Left", "Reward Group ID"]
+    table.field_names = ["ID", "Region", "Level", "Despawn Time", "Time Left", "RewardID", "1st Prize"]
     current_ids = []
+
 
     for region in data["regions"]:
         region_name = REGION_MAP.get(region['region'], "Unknown")
         for battlefield in region["battlefields"]:
-            gen_time, disp_time, time_left_str = calculate_time_details(battlefield["generatedTime"], battlefield["disappearedTime"])
-            table.add_row([battlefield['id'], f"{region['region']} - {region_name}", battlefield['level'], gen_time, disp_time, time_left_str, battlefield['rewardGroupId']])
+            disp_time, time_left_str = calculate_time_details(battlefield["disappearedTime"])
+            reward_group_id = battlefield['rewardGroupId']
+            # Check if the reward group ID exists and if it has entries
+            if str(reward_group_id) in rewards_data and rewards_data[str(reward_group_id)]:
+                first_place_prize = ', '.join(rewards_data[str(reward_group_id)][0])
+            else:
+                first_place_prize = "No data available"
+            
+            table.add_row([battlefield['id'], f"{region_name}", battlefield['level'], disp_time, time_left_str, reward_group_id, first_place_prize])
             current_ids.append(battlefield['id'])
-    
+
     print(table)
-    
+
     new_mobs = [mob_id for mob_id in current_ids if mob_id not in previous_ids]
     if new_mobs:
         print("New mobs have spawned:", new_mobs)
-        alert_for_new_mobs(new_mobs, data, min_time_left, reward_ids, telegram_alerts_enabled)
+        alert_for_new_mobs(new_mobs, data, min_time_left, reward_ids, telegram_alerts_enabled, rewards_data)
 
     return current_ids
+
 
 def main():
     """Main execution function."""
@@ -122,8 +141,9 @@ def main():
     try:
         while True:
             data = get_battlefields()
-            previous_ids = print_battlefield_info(data, previous_ids, min_time_left, reward_ids, telegram_alerts_enabled)
+            previous_ids = print_battlefield_info(data, previous_ids, min_time_left, reward_ids, telegram_alerts_enabled, rewards_data)
 
+            
             current_time = datetime.now()
             print("Last Updated:", current_time.strftime("%H:%M:%S"))
             time.sleep(300)  # Sleep for 10 minutes
